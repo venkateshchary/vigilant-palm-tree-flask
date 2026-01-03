22fb5b3b060d2bde62f2c3c0062c5eca8215f807
shashank <pshashank3@gmail.com>
2026-01-03 19:06:22 +0530

    invoice-apl

--- app.py
+++ app.py
@@ -22,6 +22,9 @@ def hello_world():
         db_version = cur.fetchone()
         cur.close()
         conn.close()
+        if db_version is None:
+            return "<p>Error: no result from SELECT version()</p>"
+
         return f"<p>Hello, World! Connected to: {db_version[0]}</p>"
     except Exception as e:
         return f"<p>Error connecting to database: {e}</p>"
@@ -77,6 +80,32 @@ def get_album_id(album_id):
         cur.close()
         conn.close()
         return f"<p>Albums: {albums}</p>"
+    except Exception as e:
+        return f"<p>Error connecting to database: {e}</p>"
+
+@app.route("/invoice", methods=['GET'])
+def get_invoice():
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM invoice')
+        invoices = cur.fetchall()
+        cur.close()
+        conn.close()
+        return f"<p>Invoices: {invoices}</p>"
+    except Exception as e:
+        return f"<p>Error connecting to database: {e}</p>"
+
+@app.route("/invoice/<string:invoice_id>", methods=['GET'])
+def get_invoice_id(invoice_id):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM invoice WHERE Invoice_ID = %s', (invoice_id,))
+        invoices = cur.fetchall()
+        cur.close()
+        conn.close()
+        return f"<p>Invoices: {invoices}</p>"
     except Exception as e:
         return f"<p>Error connecting to database: {e}</p>"
 '''
